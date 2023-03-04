ce68e16c1f858b7d712211fac70db211b9380dcf
иван матвеев <ivat1056@gmail.com>
2023-03-04 13:13:03 +0300

    For, class

--- MatveevWPFSessia1/Pages/MainPage.xaml.cs
+++ MatveevWPFSessia1/Pages/MainPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using MatveevWPFSessia1.Class;
-using MatveevWPFSessia1.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,18 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MatveevWPFSessia1
+namespace MatveevWPFSessia1.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
-            FrameClass.frame = frame;
-            FrameClass.frame.Navigate(new MainPage());
         }
     }
 }
